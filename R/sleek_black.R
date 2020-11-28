bf05c54c2afca1affc5b99a9ed06a74992ff390a
Basil Khuder <36273231+basilkhuder@users.noreply.github.com>
2020-11-28 16:48:27 -0600

    Update sleek_black.R

--- R/sleek_black.R
+++ R/sleek_black.R
@@ -1,11 +1,12 @@
 sleek_black <- function(code_file = NULL){
+  
   html <- system.file("rmarkdown/templates/sleek_black/skeleton/sleek_black.html", package = "RMDExtra")
   css <- system.file("rmarkdown/templates/sleek_black/skeleton/style.css", package = "RMDExtra")
   
   rmarkdown::html_document(toc = TRUE,
-                             toc_float = TRUE,
-                             theme = "default",
-                             css = css,
-                             highlight = "kate",
-                             template = html)
+                           toc_float = TRUE,
+                           theme = "default",
+                           css = css,
+                           highlight = "kate",
+                           template = html)
 }
